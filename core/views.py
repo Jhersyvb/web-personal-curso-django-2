0f1e5c91609a60e8b0f4860752edf799dcdaebd9
Jhersy <jhersyvb@gmail.com>
2019-10-31 19:18:55 -0500

    009 Extendiendo la App Core

--- core/views.py
+++ core/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+</ul>
+"""
+
 def home(request):
-    return HttpResponse('<h1>Mi web personal</h1>')
+    return HttpResponse(html_base + """
+        <h2>Portada</h2>
+        <p>Esto es la portada</p>
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Jhersy y soy programador</p>
+    """)
