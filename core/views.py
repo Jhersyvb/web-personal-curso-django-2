856f3174f0f6c05b1a655b1b94b7f90e629c6d30
Jhersy <jhersyvb@gmail.com>
2019-10-31 19:27:33 -0500

    011 Solucion paginas portafolio y contacto

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ html_base = """
 <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portfolio/">Portafolio</a></li>
+    <li><a href="/contact/">Contacto</a></li>
 </ul>
 """
 
@@ -18,4 +20,16 @@ def about(request):
     return HttpResponse(html_base + """
         <h2>Acerca de</h2>
         <p>Me llamo Jhersy y soy programador</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Portafolio</h2>
+        <p>Algunos de mis trabajos</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí les dejo mi correo para preguntarme dudas: jhersyvb@gmail.com</p>
     """)
